eef47f6ec3efaaf9abb5a3cdaa0894dc866623d7
dlmacedo <dlm@cin.ufpe.br>
2022-07-09 00:30:53 -0300

    eee

--- data_loader.py
+++ data_loader.py
@@ -55,30 +55,11 @@ def getCIFAR100(batch_size, TF, data_root='data', TTF=None, train=True, val=True
     return ds
 
 
-def getTINYIMAGENET(batch_size, TF, data_root='data', train=True, val=True, **kwargs):
-    data_root = os.path.expanduser(os.path.join(data_root, 'tiny-imagenet-200'))
-    kwargs.pop('input_size', None)
-
-    ds = []
-    if train:
-        train_loader = torch.utils.data.DataLoader(
-            ImageFolder(os.path.join(data_root, 'train'), transform=TF), batch_size=batch_size, shuffle=True)
-        ds.append(train_loader)
-    if val:
-        test_loader = torch.utils.data.DataLoader(
-            ImageFolder(os.path.join(data_root, 'val'), transform=TF), batch_size=batch_size, shuffle=False)
-        ds.append(test_loader)
-    ds = ds[0] if len(ds) == 1 else ds
-    return ds
-
-
 def getTargetDataSet(args, data_type, batch_size, input_TF, dataroot):
     if data_type == 'cifar10':
         train_loader, test_loader = getCIFAR10(batch_size=batch_size, TF=input_TF, data_root=dataroot, num_workers=1)
     elif data_type == 'cifar100':
         train_loader, test_loader = getCIFAR100(batch_size=batch_size, TF=input_TF, data_root=dataroot, num_workers=1)
-    elif data_type == 'tinyimagenet':
-        train_loader, test_loader = getTINYIMAGENET(batch_size=batch_size, TF=input_TF, data_root=dataroot, num_workers=1)
     return train_loader, test_loader
 
 
