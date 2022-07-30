40936c132dfff5160831f2bf4dd154a57bc1ab86
dlmacedo <dlm@cin.ufpe.br>
2022-07-30 08:17:01 -0300

    moving from 5 to 10 runs per experiment

--- run_cifar100_resnet34.sh
+++ run_cifar100_resnet34.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~softmax_std_std_std" -gpu 0 -x 5
-python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~isomax_std_std_std" -gpu 0 -x 5
-python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~isomaxplus_std_std_std" -gpu 0 -x 5
-python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~dismax_std_std_std" -gpu 0 -x 5
-python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~dismax_std_std_fpr" -gpu 0 -x 5
+python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~softmax_std_std_std" -gpu 0 -x 10
+python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~isomax_std_std_std" -gpu 0 -x 10
+python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~isomaxplus_std_std_std" -gpu 0 -x 10
+python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~dismax_std_std_std" -gpu 0 -x 10
+python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+loss~dismax_std_std_fpr" -gpu 0 -x 10
 
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~cifar100+model~resnet34+loss~softmax_std_std_std" -gpu 0
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~cifar100+model~resnet34+loss~isomax_std_std_std" -gpu 0
@@ -12,18 +12,18 @@ python main.py -ei="all" -et "train_classify" -ec "data~cifar100+model~resnet34+
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~cifar100+model~resnet34+loss~dismax_std_std_std" -gpu 0
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~cifar100+model~resnet34+loss~dismax_std_std_fpr" -gpu 0
 
-python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 5
+python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 10
 
-python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 5
+python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 10
 
 # if dismax was trained with precompute_thresholds=True, the code bellow may be used to verify them.
-#python verify.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 5
-#python verify.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 5
+#python verify.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 10
+#python verify.py --dir all --dataset cifar100 --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 10
--- run_tinyimagenet_resnet34.sh
+++ run_tinyimagenet_resnet34.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~softmax_std_std_std" -gpu 0 -x 5 -e 90 -lrde "30 60"
-python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~isomax_std_std_std" -gpu 0 -x 5 -e 90 -lrde "30 60"
-python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~isomaxplus_std_std_std" -gpu 0 -x 5 -e 90 -lrde "30 60"
-python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~dismax_std_std_std" -gpu 0 -x 5 -e 90 -lrde "30 60"
-python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~dismax_std_std_fpr" -gpu 0 -x 5 -e 90 -lrde "30 60"
+python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~softmax_std_std_std" -gpu 0 -x 10 -e 90 -lrde "30 60"
+python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~isomax_std_std_std" -gpu 0 -x 10 -e 90 -lrde "30 60"
+python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~isomaxplus_std_std_std" -gpu 0 -x 10 -e 90 -lrde "30 60"
+python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~dismax_std_std_std" -gpu 0 -x 10 -e 90 -lrde "30 60"
+python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resnet34+loss~dismax_std_std_fpr" -gpu 0 -x 10 -e 90 -lrde "30 60"
 
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~tinyimagenet+model~resnet34+loss~softmax_std_std_std" -gpu 0
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~tinyimagenet+model~resnet34+loss~isomax_std_std_std" -gpu 0
@@ -12,18 +12,18 @@ python main.py -ei="all" -et "train_classify" -ec "data~tinyimagenet+model~resne
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~tinyimagenet+model~resnet34+loss~dismax_std_std_std" -gpu 0
 #python main.py -ei="all" -et "extract_ood_logits_metrics" -ec "data~tinyimagenet+model~resnet34+loss~dismax_std_std_fpr" -gpu 0
 
-python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 5
-python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 5
+python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 10
+python calibrate.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 10
 
-python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 5
-python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 5
+python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss softmax_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomax_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss isomaxplus_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 10
+python detect.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 10
 
 # if dismax was trained with precompute_thresholds=True, the code bellow may be used to verify them.
-#python verify.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 5
-#python verify.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 5
+#python verify.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_std --gpu 0 -x 10
+#python verify.py --dir all --dataset tinyimagenet --net_type resnet34 --loss dismax_std_std_fpr --gpu 0 -x 10
